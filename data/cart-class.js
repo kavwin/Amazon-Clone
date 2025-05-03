b5bc156ff7b03e121dafd3538a017aac4a887479
kavin <kavinamutha619@gmail.com>
2025-05-03 12:39:42 +0530

    make localstoragekey and load storage method private

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,16 +2,16 @@ import {renderCheckoutHeaderQuantity} from '../scripts/Checkout/checkoutHeader.j
 
 class Cart{
     cartItems;
-    localStorageKey;
+    #localStorageKey;
 
     constructor(localStorageKey){
-        this.localStorageKey=localStorageKey;       
-        this.loadFromStorage();
+        this.#localStorageKey=localStorageKey;       
+        this.#loadFromStorage();
 
     }
 
-    loadFromStorage(){
-        this.cartItems= JSON.parse(localStorage.getItem(this.localstoragekey));
+    #loadFromStorage(){
+        this.cartItems= JSON.parse(localStorage.getItem(this.#localStorageKey));
 
         if(!this.cartItems){
             this.cartItems=[{
@@ -27,7 +27,7 @@ class Cart{
         }
     }
     saveToLocalStorage(){
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
     addToCart(productId){
         const quantitySelector=document.querySelector(`.js-quantity-selector-${productId}`);
